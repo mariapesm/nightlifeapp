f8bcfee266cb3d1ec05d998d5fedccaffb3bfee3
mariapesm <youandmecanyousee@hotmail.com>
2018-12-31 18:57:13 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, function(err) {
+mongoose.connect(process.env.mongodb://marype:networkengineering12@ds145694.mlab.com:45694/nightrate, function(err) {
   if (err) {
     console.log(err);
   } else {
